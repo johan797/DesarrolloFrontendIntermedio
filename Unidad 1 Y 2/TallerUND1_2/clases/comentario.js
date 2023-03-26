f9b354c508594af4b0546ac7380acf4d4d83ee72
johan herrera <johanherrera797@gmail.com>
2023-03-26 15:23:45 -0500

    clase_comentario v3

--- Unidad 1 Y 2/TallerUND1_2/clases/comentario.js
+++ Unidad 1 Y 2/TallerUND1_2/clases/comentario.js
@@ -1,29 +1,79 @@
 class comentario {
+    
     constructor(nombre, fecha, comentarios) {
         //validacion de argumentos
-        this. usuario = nombre;
+
+        this.usuario = nombre;
         this.fecha = fecha;
         this.texto = comentarios;
+
     }
     //metodos de clase
     agregarComentario() {
+        /*
         document.getElementById("nombre").innerHTML = this.usuario;
         document.getElementById("fecha").innerHTML = this.fecha;
         document.getElementById("texto").innerHTML = this.texto;
-      }
-    
-      editarComentario(usuario, fecha, texto) {
+*/
+        // Crea un nuevo elemento div para el comentario
+        let comentarioDiv = document.createElement("div");
+        comentarioDiv.classList.add("comentario");
+
+        // Crea nuevos elementos HTML para mostrar el nombre, fecha y comentario
+        let nombreElement = document.createElement("span");
+        nombreElement.classList.add("nombre");
+        nombreElement.textContent = this.usuario;
+
+        let fechaElement = document.createElement("p");
+        fechaElement.classList.add("fecha");
+        fechaElement.textContent = this.fecha;
+
+        let textoElement = document.createElement("p");
+        textoElement.classList.add("texto");
+        textoElement.textContent = this.texto;
+
+        let editarElement = document.createElement("button");        
+        editarElement.classList.add("editar");
+        editarElement.innerHTML = 'editar';
+
+        let borrarElement = document.createElement("button");        
+        borrarElement.classList.add("borrar");
+        borrarElement.innerHTML = 'Borrar';
+        
+        // Agrega los nuevos elementos al comentarioDiv
+        comentarioDiv.appendChild(nombreElement);
+        comentarioDiv.appendChild(fechaElement);
+        comentarioDiv.appendChild(textoElement);
+        comentarioDiv.appendChild(editarElement);
+        comentarioDiv.appendChild(borrarElement);
+
+        // Agrega el comentarioDiv al contenedor de comentarios
+        document.getElementById("comentarios").appendChild(comentarioDiv);
+    }
+    ocultarMostrarElemento() {
+        let elemento1 = document.getElementById("editar");
+        let elemento2 = document.getElementById("borrar");
+
+        if (elemento1.style.display == "none" & elemento2.style.display == "none") {
+            elemento1.style.display = "block";
+            elemento2.style.display = "block";
+        } else {
+            elemento1.style.display = "none";
+            elemento2.style.display = "none";
+        }
+    }
+    editarComentario(usuario, fecha, texto) {
         this.usuario = usuario;
         this.fecha = fecha;
         this.texto = texto;
         this.agregarComentario();
-      }
-    
-      eliminarComentario() {
+    }
+
+    eliminarComentario() {
         this.usuario = "";
         this.fecha = "";
         this.texto = "";
         this.agregarComentario();
-      }
     }
+}
 
--- Unidad 1 Y 2/TallerUND1_2/index.js
+++ Unidad 1 Y 2/TallerUND1_2/index.js
@@ -1,17 +1,29 @@
+function fechahoy (){
+    var today = new Date(); 
+    // `getDate()` devuelve el día del mes (del 1 al 31)
+    var day = today.getDate().toString().padStart(2, "0");     
+    // `getMonth()` devuelve el mes (de 0 a 11)
+    var month = (today.getMonth() + 1).toString().padStart(2, "0");     
+    // `getFullYear()` devuelve el año completo
+    var year = today.getFullYear();
 
-
+    return (`${day}-${month}-${year}`)
+}
 
 // Agregar controlador de eventos al formulario
 document.getElementById("comentario-form").addEventListener("submit", function (event) {
     // Evitar que la página se recargue al enviar el formulario
     event.preventDefault();
-    // Llamar a la función imprimirComentario()
-    let names = document.getElementById("nombre-input").value;
-    let date = document.getElementById("fecha-input").value;
+    let names = document.getElementById("nombre-input").value;    
+    let date =fechahoy();
     let comments = document.getElementById("comentario-input").value;
+
     let comentario1 = new comentario(names,date,comments);
-    comentario1.agregarComentario();   
+    comentario1.agregarComentario();
+    
 });
+
+
 /*
 function imprimirComentario() {
     let nombreUsuario = document.getElementById("nombre-input").value;
