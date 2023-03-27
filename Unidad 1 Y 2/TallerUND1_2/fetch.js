2a4183fa569070a8e68161394f0ed439b6ba28de
johan herrera <johanherrera797@gmail.com>
2023-03-26 23:10:34 -0500

    consumoApi

--- Unidad 1 Y 2/TallerUND1_2/fetch.js
+++ Unidad 1 Y 2/TallerUND1_2/fetch.js
@@ -7,6 +7,7 @@ const options = {
 };
 
 fetch('https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=tt0001702%2Ctt0001856%2Ctt0001856', options)
+    // Convertir la respuesta de la solicitud a un objeto JSON
     .then(response => response.json())
     .then(response => {
         /*// Obtener el elemento donde se mostrará la respuesta
@@ -14,14 +15,20 @@ fetch('https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=tt00
         // Asignar el valor de la respuesta al elemento
         respuestaElement.innerHTML = JSON.stringify(response, null, 3);*/
 
+        // Obtener los datos de la respuesta y asignarlos a una variable llamada "data"
         const data = response;
-
+        // Obtener la tabla y la sección de cuerpo de la tabla
         const table = document.getElementById("grid");
         const tbody = table.querySelector("tbody");
 
+        /*el método forEach() para iterar sobre los 
+        resultados del JSON y agregar cada fila correspondiente a la tabla. 
+        Para cada resultado, se crean las celdas correspondientes en la fila y
+         se asignan los valores*/
         data.results.forEach(result => {
+            // Crear una nueva fila en la sección de cuerpo de la tabla
             const row = tbody.insertRow();
-
+            // Agregar una celda para el ID del resultado
             const idCell = row.insertCell();
             idCell.textContent = result.id;
 
