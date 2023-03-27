252cc02f1eac3548dc126c9a2fe980d40310047c
johan herrera <johanherrera797@gmail.com>
2023-03-26 22:51:30 -0500

    consumoApi

--- Unidad 1 Y 2/TallerUND1_2/fetch.js
+++ Unidad 1 Y 2/TallerUND1_2/fetch.js
@@ -9,9 +9,41 @@ const options = {
 fetch('https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=tt0001702%2Ctt0001856%2Ctt0001856', options)
     .then(response => response.json())
     .then(response => {
-        // Obtener el elemento donde se mostrará la respuesta
+        /*// Obtener el elemento donde se mostrará la respuesta
         const respuestaElement = document.getElementById('respuesta-fetch');
         // Asignar el valor de la respuesta al elemento
-        respuestaElement.innerHTML = JSON.stringify(response);
+        respuestaElement.innerHTML = JSON.stringify(response, null, 3);*/
+
+        const data = response;
+
+        const table = document.getElementById("grid");
+        const tbody = table.querySelector("tbody");
+
+        data.results.forEach(result => {
+            const row = tbody.insertRow();
+
+            const idCell = row.insertCell();
+            idCell.textContent = result.id;
+
+            const imageCell = row.insertCell();
+            if (result.primaryImage && result.primaryImage.url) {
+                const image = document.createElement("img");
+                image.src = result.primaryImage.url;
+                image.alt = result.titleText.text;
+                imageCell.appendChild(image);
+            }
+
+            const typeCell = row.insertCell();
+            typeCell.textContent = result.titleType.text;
+
+            const titleCell = row.insertCell();
+            titleCell.textContent = result.titleText.text;
+
+            const yearCell = row.insertCell();
+            yearCell.textContent = result.releaseYear.year;
+
+            const dateCell = row.insertCell();
+            dateCell.textContent = `${result.releaseDate.day}/${result.releaseDate.month}/${result.releaseDate.year}`;
+        });
     })
     .catch(err => console.error(err));
