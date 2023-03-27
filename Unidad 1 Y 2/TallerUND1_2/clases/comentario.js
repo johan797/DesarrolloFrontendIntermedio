8ee39340b2a59c97d613bac41a63981e9f828af5
johan herrera <johanherrera797@gmail.com>
2023-03-26 21:53:32 -0500

    clase_comentario_v5

--- Unidad 1 Y 2/TallerUND1_2/clases/comentario.js
+++ Unidad 1 Y 2/TallerUND1_2/clases/comentario.js
@@ -1,24 +1,32 @@
 class comentario {
     
-    constructor(nombre, fecha, comentarios) {
+    constructor(id,nombre, fecha, comentarios) {
         //validacion de argumentos
-
+        this.id = id;
         this.usuario = nombre;
         this.fecha = fecha;
         this.texto = comentarios;
 
     }
+
+    borrarComentario(id) {
+        // Busca el comentario correspondiente al id
+        let comentarioDiv = document.querySelector(`.comentario[id='${id}']`);
+        if (!comentarioDiv) {
+          console.error(`Comentario no encontrado para id ${id}`);
+          return;
+        }
+        // Elimina el comentario
+        comentarioDiv.remove();
+        console.log(`Comentario ${id} eliminado`);
+        }
     //metodos de clase
     agregarComentario() {
-        /*
-        document.getElementById("nombre").innerHTML = this.usuario;
-        document.getElementById("fecha").innerHTML = this.fecha;
-        document.getElementById("texto").innerHTML = this.texto;
-*/
+
         // Crea un nuevo elemento div para el comentario
         let comentarioDiv = document.createElement("div");
         comentarioDiv.classList.add("comentario");
-
+        comentarioDiv.id='com'+this.id;
         // Crea nuevos elementos HTML para mostrar el nombre, fecha y comentario
         let nombreElement = document.createElement("span");
         nombreElement.classList.add("nombre");
@@ -32,13 +40,16 @@ class comentario {
         textoElement.classList.add("texto");
         textoElement.textContent = this.texto;
 
+        // Crea nuevos botones para editar y borrar el comentario
         let editarElement = document.createElement("button");        
-        editarElement.classList.add("editar");
-        editarElement.innerHTML = 'editar';
+        editarElement.classList.add("editar");                 
+        editarElement.innerHTML = 'Editar';
 
         let borrarElement = document.createElement("button");        
-        borrarElement.classList.add("borrar");
+        borrarElement.classList.add("borrar");               
         borrarElement.innerHTML = 'Borrar';
+        borrarElement.id=this.id;
+        
         
         // Agrega los nuevos elementos al comentarioDiv
         comentarioDiv.appendChild(nombreElement);
@@ -46,23 +57,21 @@ class comentario {
         comentarioDiv.appendChild(textoElement);
         comentarioDiv.appendChild(editarElement);
         comentarioDiv.appendChild(borrarElement);
-
         // Agrega el comentarioDiv al contenedor de comentarios
         document.getElementById("comentarios").appendChild(comentarioDiv);
+
     }
 
-    editarComentario(usuario, fecha, texto) {
-        this.usuario = usuario;
-        this.fecha = fecha;
-        this.texto = texto;
-        this.agregarComentario();
+    
+/*
+    borrarComentario(id) {
+        // Busca el comentario correspondiente al id
+    let comentarioDiv = document.getElementById(`com${id}`);
+    if (!comentarioDiv) {
+        console.error(`No se encontró el comentario con id ${id}`);
+        return;
     }
-
-    eliminarComentario() {
-        this.usuario = "";
-        this.fecha = "";
-        this.texto = "";
-        this.agregarComentario();
-    }
+    comentarioDiv.remove();
+    console.log(`Comentario con id ${id} eliminado.`);
+    }*/
 }
-
--- Unidad 1 Y 2/TallerUND1_2/index.js
+++ Unidad 1 Y 2/TallerUND1_2/index.js
@@ -1,40 +1,49 @@
-function fechahoy (){
+function fechahoy() {
     var today = new Date(); 
-    // `getDate()` devuelve el día del mes (del 1 al 31)
     var day = today.getDate().toString().padStart(2, "0");     
-    // `getMonth()` devuelve el mes (de 0 a 11)
     var month = (today.getMonth() + 1).toString().padStart(2, "0");     
-    // `getFullYear()` devuelve el año completo
     var year = today.getFullYear();
-
-    return (`${day}-${month}-${year}`)
+    return `${day}-${month}-${year}`;
 }
 
-// Agregar controlador de eventos al formulario
+let id = 1;
+
 document.getElementById("comentario-form").addEventListener("submit", function (event) {
-    // Evitar que la página se recargue al enviar el formulario
     event.preventDefault();
     let names = document.getElementById("nombre-input").value;    
-    let date =fechahoy();
+    let date = fechahoy();
     let comments = document.getElementById("comentario-input").value;
-
-    let comentario1 = new comentario(names,date,comments);
-    comentario1.agregarComentario();
-    
+    let ids = id.toString();
+    let comentario1 = new comentario(ids, names, date, comments);
+    comentario1.agregarComentario(); 
+    id += 1;
 });
 
 
+document.getElementById("comentarios").addEventListener("click", function (event) {
+    let target = event.target;
+        let id = 'com'+target.id;
+        console.log(id);
+        let comentarios = new comentario(id, null, null, null);
+        comentarios.borrarComentario(id);    
+});
 /*
-function imprimirComentario() {
-    let nombreUsuario = document.getElementById("nombre-input").value;
-    let fecha = document.getElementById("fecha-input").value;
-    let comentario = document.getElementById("comentario-input").value;
-
-    document.getElementById("nombre").innerHTML = nombreUsuario;
-    document.getElementById("fecha").innerHTML = fecha;
-    document.getElementById("texto").innerHTML = comentario;
-}
+document.getElementById("comentarios").addEventListener("click", function (event) {
+    let target = event.target;
+    if (target.classList.contains("editar")) {
+        let id = target.dataset.num;
+        let comentarios = new comentario(id, null, null, null);
+        comentarios.editarComentario(id);
+    } else if (target.classList.contains("borrar")) {
+        let id = target.dataset.num;
+        let comentarios = new comentario(id, null, null, null);
+        comentarios.borrarComentario(`com${id}`);
+    }
+});
 */
+
+
+
 /*
 
 
